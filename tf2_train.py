7c4fd1975035ea2327cab5460d798289f5f8a563
Sam Freitas <samfreitas@email.arizona.edu>
2020-12-09 15:04:30 -0700

    small update

--- tf2_train.py
+++ tf2_train.py
@@ -22,8 +22,8 @@ data_dir2 = pathlib.Path(os.path.join(os.getcwd(),'TFWP_training'))
 # print(image_count)
 
 batch_size = 32
-img_height = 256
-img_width = 256
+img_height = 192
+img_width = 192
 
 train_ds = tf.keras.preprocessing.image_dataset_from_directory(
     data_dir2,
@@ -52,13 +52,13 @@ AUTOTUNE = tf.data.experimental.AUTOTUNE
 train_ds = train_ds.cache().shuffle(1000).prefetch(buffer_size=AUTOTUNE)
 val_ds = val_ds.cache().prefetch(buffer_size=AUTOTUNE)
 
-normalization_layer = layers.experimental.preprocessing.Rescaling(1./255)
+# normalization_layer = layers.experimental.preprocessing.Rescaling(1./255)
 
-normalized_ds = train_ds.map(lambda x, y: (normalization_layer(x), y))
-image_batch, labels_batch = next(iter(normalized_ds))
-first_image = image_batch[0]
-# Notice the pixels values are now in `[0,1]`.
-print(np.min(first_image), np.max(first_image)) 
+# normalized_ds = train_ds.map(lambda x, y: (normalization_layer(x), y))
+# image_batch, labels_batch = next(iter(normalized_ds))
+# first_image = image_batch[0]
+# # Notice the pixels values are now in `[0,1]`.
+# print(np.min(first_image), np.max(first_image)) 
 
 num_classes = 2
 
